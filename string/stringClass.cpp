60d2bdc4abeced2d9bb331ed58f73733d5dd00c0
Navjeet Sandhu <navjeet.sandhu@gmail.com>
2024-06-23 19:45:27 -0400

    changes

--- string/stringClass.cpp
+++ string/stringClass.cpp
@@ -25,8 +25,52 @@ public:
 };
 
 
+
+class EmployeeReference
+{
+private:
+    std::string mEmployeeName;
+
+public:
+    explicit EmployeeReference(std::string&   employeeName) : mEmployeeName(employeeName) {
+
+    }
+    virtual ~EmployeeReference() = default;
+
+    const std::string& getName() const {
+        return mEmployeeName;
+    }
+};
+
+class CoopStudentReference: public EmployeeReference
+{
+public:
+    explicit CoopStudentReference(std::string& employeeName) : EmployeeReference(employeeName) {}
+};
+
+
+void  testStringMove()
+{
+    std::string str1 = "Hello, world!";
+    std::string str2 = std::move(str1);
+    std::cout <<"str1: " << str1 << " str2:  " << str2 << std::endl;
+
+}
+
+void testStringReferenceClass()
+{
+    std::string str1 = "John  Reference";
+    CoopStudentReference coop(str1);
+    std::cout << coop.getName() << std::endl;
+}
+
+
 void testStringClass()
 {
     CoopStudent coop("John");
-    std::cout << coop.getName();
+    std::cout << coop.getName() << std::endl;
+    testStringMove();
+    testStringReferenceClass();
+
 }
+
