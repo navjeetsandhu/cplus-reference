ca18b513967312d65472359d520d549f3475e2e9
Navjeet Sandhu <navjeet.sandhu@gmail.com>
2024-06-25 06:38:24 -0400

    changes

--- threads/twoThread.cpp
+++ threads/twoThread.cpp
@@ -8,33 +8,28 @@ std::condition_variable cond;      // Shared condition variable, wait, wait_for,
 int shared_value = 0;   // Shared resource
 
 
-
-void thread_one() {
-    std::cout << "starting thread one" << std::endl;
+void thread_one(std::string&& name, int value) {
+    std::cout << "starting thread " << name << std::endl;
     std::unique_lock<std::mutex> lock(mu);
-    std::cout << "waiting thread one for condition" << std::endl;
-    cond.wait(lock, []{ return shared_value ==1; });
-    std::cout << "waiting ended for thread one for condition" << std::endl;
 
-    std::cout << "Finishing thread one" << std::endl;
+    
+    std::cout << "waiting thread "  << name << " or condition " << std::endl;
+
+    cond.wait(lock, [value]{ return shared_value == value; });
+
+
+    std::cout << "waiting ended for thread "  << name << " for condition" << std::endl;
+
+    std::cout << "Finishing thread "  << name  << std::endl;
 
 }
 
-
-void thread_two() {
-    std::cout << "starting thread two" << std::endl;
-    std::unique_lock<std::mutex> lock(mu);
-    std::cout << "waiting thread two for condition" << std::endl;
-    cond.wait(lock, []{ return shared_value ==2; });
-    std::cout << "waiting ended for thread two for condition" << std::endl;
-
-    std::cout << "Finishing thread two" << std::endl;
-}
 
 void two_threads_with_condition() {
 
-    std::thread t1(thread_one);  // start example with 2 concurrent threads
-    std::thread t2(thread_two);
+    std::thread t1(thread_one, "thread_one", 1);  // start example with 2 concurrent threads
+    std::thread t2(thread_one, "thread_two", 2);
+    std::this_thread::sleep_for(std::chrono::seconds(4));
     std::cout << "Created two threads" << std::endl;
 
 
