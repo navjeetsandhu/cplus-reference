71bc40df6d17edb3b448cbca5f618cd51032d5ad
Navjeet Sandhu <navjeet.sandhu@gmail.com>
2024-07-02 15:02:27 -0400

    static variable

--- class/copy/copyClass.cpp
+++ class/copy/copyClass.cpp
@@ -16,13 +16,13 @@ public:
 };
 
 
-class CopyMe: public BaseMe {
+class DeriveMe: public BaseMe {
 
 private:
     std::unique_ptr<int> p;
 
 public:
-    explicit CopyMe(int count) : BaseMe(count), p(new int(count)) {
+    explicit DeriveMe(int count) : BaseMe(count), p(new int(count)) {
     }
 
     using BaseMe::getCount;
