d2dd0797b2a55f8f926e03e369df6c7590c38081
Navjeet Sandhu <navjeet.sandhu@gmail.com>
2024-07-03 11:45:57 -0400

    changes

--- class/copy/copyClass.cpp
+++ class/copy/copyClass.cpp
@@ -22,20 +22,22 @@ public:
         std::cout << "Destructor called!: " << *this;
 
     }
-
-
 };
 
 void testDeepCopy() {
     // Constructor called
     Widget w1(10);
 
-    std::cout << w1;
+    std::cout << "w1: " << w1; // w1: ( ptr: 0x6000019a0030, value: 10 )
 
     // Deep Copy
     Widget w2 = w1;
 
 
-    std::cout << w2;
+    std::cout << "w2: " << w2; // w2: ( ptr: 0x6000019a0040, value: 10 )
+
+    std::cout << "Creating constant reference w3, No copy constructor called" << std::endl;
+    const Widget& w3 = w1;
+    std::cout << "w3: " << w3;  //w3: ( ptr: 0x6000019a0030, value: 10 )
 
 }
