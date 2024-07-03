0843330c0896b04ce3411538221e15b7a53639c3
Navjeet Sandhu <navjeet.sandhu@gmail.com>
2024-07-03 11:34:42 -0400

    changes

--- class/copy/copyClass.cpp
+++ class/copy/copyClass.cpp
@@ -1,37 +1,41 @@
 #include <iostream>
 
 class Widget {
-    int* dataPtr;
+    std::unique_ptr<int> dataPtr;
 public:
     explicit Widget(int data) {
-        this->dataPtr = new int(data);
-        std::cout << "Constructor called! Value: " << *this->dataPtr << std::endl;
+        this->dataPtr = std::make_unique<int>(data);
+        std::cout << "Constructor called!: " << *this;
     }
     // Implement deep copy in copy constructor
     Widget(const Widget& w) {
-        this->dataPtr = new int(*w.dataPtr);
-        std::cout << "Copy constructor called! Value: " << *this->dataPtr << std::endl;
+        this->dataPtr = std::make_unique<int>(*w.dataPtr);
+        std::cout << "Copy constructor called!: " << *this;
     }
 
-    ~Widget() {\
-        std::cout << dataPtr << " Destructor called!" << std::endl;
-        delete dataPtr;
+    friend std::ostream& operator << (std::ostream& os, const Widget& w) {
+        os << "( ptr: " << w.dataPtr << ", value: " << *w.dataPtr << " )" <<std::endl;
+        return os;
+    }
+
+    ~Widget() {
+        std::cout << "Destructor called!: " << *this;
 
     }
 
-    void print() {
-        std::cout << dataPtr << " Data value: " << *dataPtr << std::endl;
-    }
+
 };
 
 void testDeepCopy() {
     // Constructor called
     Widget w1(10);
 
+    std::cout << w1;
+
     // Deep Copy
     Widget w2 = w1;
 
-    w1.print(); // prints 10
-    w2.print(); // prints 10
+
+    std::cout << w2;
 
 }
